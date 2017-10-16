1f57087170b0d5943eb172c99f129e08e79b0065
Magda Mielczarek <maga.mielczarek@gmail.com>
2017-10-16 13:52:24 +0200

    wersja do weryfikacji

--- masteralb.js
+++ masteralb.js
@@ -1,3 +1,5 @@
+// checkbox-thick
+
 const checkbox_collection = document.querySelectorAll("input[type='checkbox']");
 const thick_box = document.querySelectorAll(".thick-box");
 thick_box.forEach(function(element){
--- validation.js
+++ validation.js
@@ -1,130 +1,90 @@
 const forms = document.querySelectorAll("form[data-validate]");
-const validated_inputs = document.querySelectorAll("form[data-validate] input[data-validate-rules]");
-
-	const validate_type = {
-		reset_validation: function(element){
-			element.parentElement.classList.remove("error-field");
-			element.parentElement.classList.remove("error-checkbox");
-			//document.querySelectorAll(".validation_info").forEach(function(element){element.innerHTML=""});
-		},
-		required: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			validate_info.innerText="";
-			if(/^\s*$/.test(element.value)==true){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field is required</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			} 
-		},
-		minimum_length_5: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(element.value.length<5){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field must not contain less than 5 signs</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			} 
-		},
-		max_length_15: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(element.value.length>15){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field must not contain more than 15 signs</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			} 
-		},
-		max_length_30: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(element.value.length>30){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field must not contain more than 30 signs</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			} 
-		},
-		email_pattern: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(element.value!=""&&element.value.search(/^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/)==-1||element.value==0){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>Invalid email pattern</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			} 
-		},
-		contain_number: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(element.value.search( /\d{1}/)==-1){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>Password must contain at least one digit</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			} 
-		},
-		no_special_char: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(/[\-\[\]\/\\,\\\{\}\(\)\*\+\?\.\^\$\|]/.test(element.value)==true){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field should not contain any special characters or symbols</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			} 
-		},
-		same_password: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(element.value!==document.getElementById('password').value){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This is not your password</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			}
-		},
-		checked: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			validate_info.innerText="";
-			if(element.getAttribute("checked")!=="checked"){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>You have to accept the terms of use</p>");
-				element.parentElement.classList.add("error-checkbox");
-			} else {
-				return;
-			}
-		},
-		selected: function(element){
-			const validate_info = element.parentNode.nextElementSibling;
-			if(element.getAttribute.checked=="checked"){
-				validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field should not contain more than 40 characters</p>");
-				element.parentElement.classList.add("error-field");
-			} else {
-				if(element.parentElement.classList=="error-field"){
-					return;
-				}
-			}
+const validated_inputs = document.querySelectorAll("form[data-validate] input[data-validate-rules],form[data-validate] select[data-validate-rules],form[data-validate] textarea[data-validate-rules]");
+const validate_type = {
+	reset_validation: function(element){
+		element.parentElement.classList.remove("error-field");
+		element.parentElement.classList.remove("error-checkbox");
+		element.parentNode.nextElementSibling.innerHTML="";
+	},
+	required: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(/^\s*$/.test(element.value)==true){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field is required</p>");
+			element.parentElement.classList.add("error-field");
+		} 
+	},
+	minimum_length_5: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.value.length<5&&element.value!==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field must not contain less than 5 signs</p>");
+			element.parentElement.classList.add("error-field");
+		} 
+	},
+	max_length_15: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.value.length>15&&element.value!==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field must not contain more than 15 signs</p>");
+			element.parentElement.classList.add("error-field");
+		} 
+	},
+	max_length_30: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.value.length>30&&element.value!==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field must not contain more than 30 signs</p>");
+			element.parentElement.classList.add("error-field");
 		}
-	};
+	},
+	email_pattern: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.value.search(/^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/)==-1&&element.value!==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>Invalid email pattern</p>");
+			element.parentElement.classList.add("error-field");
+		}
+	},
+	contain_number: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.value.search( /\d{1}/)==-1&&element.value!==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>Password must contain at least one digit</p>");
+			element.parentElement.classList.add("error-field");
+		}
+	},
+	no_special_char: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(/[\-\[\]\/\\,\\\{\}\(\)\*\+\?\.\^\$\|]/.test(element.value)==true&&element.value!==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This field should not contain any special characters or symbols</p>");
+			element.parentElement.classList.add("error-field");
+		}
+	},
+	same_value: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.value!==document.getElementById('password').value&&element.value!==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>This is not your password</p>");
+			element.parentElement.classList.add("error-field");
+		}
+	},
+	checked: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.getAttribute("checked")!=="checked"){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>You have to accept the terms of use</p>");
+			element.parentElement.classList.add("error-checkbox");
+		}
+	},
+	selected: function(element){
+		const validate_info = element.parentNode.nextElementSibling;
+		if(element.value==""){
+			validate_info.insertAdjacentHTML("beforeend","<p class='validation_details'>You have to choose one option</p>");
+			element.parentElement.classList.add("error-field");
+		}
+	},
+};
 
+// containers for validation info
 validated_inputs.forEach(function(element){element.parentNode.insertAdjacentHTML("afterend","<div class='validation_info'></div>")});
 
-// validation on change for each input (with data validate rules)
+// validation on blur for each input
 
 for(let i=0;i<validated_inputs.length;i++){
-	validated_inputs[i].addEventListener("change", function(){
+	validated_inputs[i].addEventListener("blur", function(){
 		const attributes = validated_inputs[i].getAttribute("data-validate-rules").split(",");
 		for(let index=0;index<attributes.length;index++){
 			if(typeof validate_type[attributes[index]]==="function"){
@@ -137,10 +97,12 @@ for(let i=0;i<validated_inputs.length;i++){
 // validation on submit
 
 forms.forEach(function(element){
-	const inputs_to_final_validation = element.querySelectorAll("input[data-validate-rules]");
+	const inputs_to_final_validation = element.querySelectorAll("input[data-validate-rules],select[data-validate-rules],textarea[data-validate-rules]");
 	const submit_button = element.querySelector("input[type=submit]");
 	submit_button.addEventListener("click",function(event){
 		event.preventDefault();
+		element.querySelectorAll(".validation_info").forEach(function(element){ //reset
+		element.innerHTML="";});
 		for(let i=0;i<inputs_to_final_validation.length;i++){
 			const attributes = inputs_to_final_validation[i].getAttribute("data-validate-rules").split(",");
 			for(let index=0;index<attributes.length;index++){
@@ -148,17 +110,6 @@ forms.forEach(function(element){
 				validate_type[attributes[index]](inputs_to_final_validation[i]);
 				};
 			};
-		};
-		let is_valid;
-		document.querySelectorAll(".validation_info").forEach(function(element){
-			if(element.textContent!=""){
-				is_valid = false;
-			};
-		});
-		if(is_valid == false){
-			alert("formularz zawiera puste lub nieprawidłowo wypełnione pola")
-		} else {alert("formularz gotowy do wysłania")
-		//miejsce na kod z funkcją wysyłającą formularz na serwer
 		};
 	});
 });
